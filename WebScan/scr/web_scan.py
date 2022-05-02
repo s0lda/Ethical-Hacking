856bfb12196f17896fa2d086a5b3fc734d4dcfe0
Jakub <soldzik@gmail.com>
2022-05-03 00:49:20 +0100

    ,

--- WebScan/scr/web_scan.py
+++ WebScan/scr/web_scan.py
@@ -1,18 +1,17 @@
+from typing import Any
 from kivymd.app import MDApp
 from kivy.uix.relativelayout import RelativeLayout
 from kivy.core.window import Window
 from scr.screen_size import get_screen_size
 from scr.port_scanner import PortScanner
 from scr.domain_lookup import DomainLookUp
-
+from kivy.clock import Clock
 
 class WebScan(RelativeLayout):
-    
-    def reset_progress_bar(self) -> None:
-        self.ids.progress_bar.value = 0
-        self.ids.progress_bar.max = 0
-        self.ids.progress_bar.min = 0
-        self.ids.progress_bar.opacity = 0
+    start_port = 0
+    end_port = 0
+    current_port = 1
+    ip = ''
 
     def check_domain(self, domain: str) -> None:
         domain_check = DomainLookUp(domain).get_domain_info()
@@ -22,29 +21,39 @@ class WebScan(RelativeLayout):
             self.ids.info_label.text = 'Domain is not registered.'
             
     def port_scan(self, ip: str, s_port: int | str, e_port: int | str) -> None:
-        self.reset_progress_bar()
+        self.ids.info_label.text = ''
         # show progress bar
         self.ids.progress_bar.opacity = 1
-        print(s_port, e_port)
-        # set start and end port if not provided
+        
         if s_port == '':
             s_port = 0
         if e_port == '':
             e_port = 1024
-        e_port = e_port if e_port > s_port else s_port # type: ignore e_port and s_port
-                                                       # are ints at this stage.
-        # set progress bar values
-        self.ids.progress_bar.max = int(e_port)
-        self.ids.progress_bar.min = int(s_port)
-        self.ids.progress_bar.value = int(s_port)
-        self.ids.info_label.text = f' PORTS {s_port}-{e_port}\n OPEN PORTS:\n'
+        e_port = int(e_port) if int(e_port) > int(s_port) else int(s_port)
         
-        scanner = PortScanner(ip)
-        for port in range(int(s_port), int(e_port) + 1):
-            self.ids.progress_bar.value += 1
-            open_port = scanner.scan_port(port)
+        self.start_port = int(s_port)
+        self.end_port = int(e_port)
+        self.current_port = self.start_port
+        self.ip = ip
+        
+        self.ids.progress_bar.max = self.end_port - self.start_port
+        
+        self.ids.info_label.text = f'Scanning Ports: {self.start_port} - {self.end_port}\nOpen Ports:\n'
+        Clock.schedule_interval(self.callback, 0.1)
+
+    def callback(self, *args: Any) -> None:
+        scanner = PortScanner(self.ip)
+        if self.current_port <= self.end_port:
+            open_port = scanner.scan_port(self.current_port)
             if open_port:
-                self.ids.info_label.text = f'{self.ids.info_label.text} {port} '
+                self.ids.info_label.text = f'{self.ids.info_label.text} {self.current_port} '
+            self.current_port += 1
+            self.ids.progress_bar.value += 1
+        else:
+            Clock.unschedule(self.callback)
+            self.ids.progress_bar.opacity = 0
+            self.ids.progress_bar.value = 0
+
 
 class Application(MDApp):
     title = 'Web Scanner'
